261afa14336401c0b2cfaf4dfae2c340a0762999
Marcus Ikegami <hikegami.6@gmail.com>
2022-02-16 19:14:14 -0700

    Updated React homepage

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,7 +18,27 @@ const Login = (props) => {
         e.preventDefault();
         login({ variables: { name: username } });
       }}>
-        <label htmlFor="username">
+        <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) =>{ 
+              setUsername(e.target.value);
+              console.log(username);
+            }}
+          />
+        
+
+        <button>Login</button>
+      </form>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        login({ variables: { name: username } });
+      }}>
+        <label htmlFor="username">Create Account</label>
           <input
             id="username"
             type="text"
@@ -26,7 +46,7 @@ const Login = (props) => {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-        </label>
+        
 
         <button>Login</button>
       </form>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,16 @@
 
 const Profile = () => {
     return (
-        <div>Hello</div>
+        <section id="Profile">
+            <h1 id="username">ExampleUsername's Profile</h1>
+            <div id="user-metrics">
+
+                <h3>Total Posts: (number)</h3>
+                <h3>Total Replies: (number)</h3>
+                <h3>Credibility: (number)</h3>
+
+            </div>
+        </section>
     )
 }
 
