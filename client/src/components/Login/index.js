133d6535ffd945abd045c4963ba1d13f920091b9
Marcus Ikegami <hikegami.6@gmail.com>
2022-02-23 18:16:09 -0700

    Updated login page and dashboard

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -2,21 +2,24 @@ import { validateEmail } from '../../utils/helpers';
 import React, { useState } from "react";
 
 function Login() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-  
+    const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+
     const [errorMessage, setErrorMessage] = useState('');
-    const { name, email, message } = formState;
+    const { username, email, password } = formState;
   
-    const handleSubmit = (event) => {
+    const handleLoginSubmit = (event) => {
       event.preventDefault();
       if (!errorMessage) {
-        setFormState({ [event.target.name]: event.target.value });
+        setFormState({username: event.target.username.value, email: event.target.email.value, password: event.target.password.value});
         console.log('Form', formState);
+        // setFormState({ username: '', email: '', password: '' });
       }
+
+      
     };
   
     const handleChange = (event) => {
-      if (event.target.name === 'email') {
+      if (event.target.username === 'email') {
         const isValid = validateEmail(event.target.value);
         if (!isValid) {
           setErrorMessage('Not a valid email.');
@@ -25,7 +28,7 @@ function Login() {
         }
       } else {
         if (!event.target.value.length) {
-          setErrorMessage(`${event.target.name} is required.`);
+          setErrorMessage(`${event.target.username} is required.`);
         } else {
           setErrorMessage('');
         }
@@ -35,25 +38,33 @@ function Login() {
     return (
       <section>
 
-        <h2 className="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-          <div className="col-12 mb-2 bg-dark text-light p-3">
-           
-                <div className="col-9 about">
-                    <ul>
-                        <li>hikegami.6@gmail.com</li>
-                        <li>(801) 598-6965</li>
-                        <li>The styling of the form functionality was not implemented because I do not intend to keep the contact form in the finished product. This small contact section will be incorporated into the about after project grading.</li>
-                    </ul>
-                </div>
-            </div>
+       
 
-        <h1 className="text-dark bg-primary p-2 display-inline-block">Contact me</h1>
+        <h1 className="text-dark bg-primary p-2 display-inline-block">Login</h1>
         <div className="col-12 mb-2 bg-dark text-light p-3">
-          <form id="contact-form" onSubmit={handleSubmit}>
+          <form id="contact-form" onSubmit={handleLoginSubmit}>
             <div>
-              <label htmlFor="name">Name:</label>
+              <label htmlFor="email">Email address:</label>
               <br/>
-              <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+              <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <br/>
+              <input type="password" name="password" defaultValue={password} onBlur={handleChange} />
+            </div>
+            
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+        <h1 className="text-dark bg-primary p-2 display-inline-block">Signup</h1>
+        <div className="col-12 mb-2 bg-dark text-light p-3">
+          <form id="contact-form" onSubmit={handleLoginSubmit}>
+            <div>
+              <label htmlFor="username">Username:</label>
+              <br/>
+              <input type="text" name="username" defaultValue={username} onBlur={handleChange} />
             </div>
             <div>
               <label htmlFor="email">Email address:</label>
@@ -61,16 +72,12 @@ function Login() {
               <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
             </div>
             <div>
-              <label htmlFor="message">Message:</label>
+              <label htmlFor="password">Password:</label>
               <br/>
-              <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-            {errorMessage && (
-              <div>
-                <p className="error-text">{errorMessage}</p>
-              </div>
-            )}
+              <input type="password" name="password" defaultValue={password} onBlur={handleChange} />
             </div>
-            <button data-testid="button" type="submit">Submit</button>
+            
+            <button type="submit">Submit</button>
           </form>
         </div>
       </section>
